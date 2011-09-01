de5d843048e72e1dce8fb17725706700f15fa24c
Westley Bonack <westley@mint>
2011-09-01 16:33:21 -0500

    I changed new_passing.cpp again but it is still giving me problems that I don't understand.  Sometimes you gotta hate programming I guess.... the next step will be to write the algorithm on paper and figure out what exactly is going wrong with it.  After this is finished it should be super readable and intuitive in its checks.

--- new_passing.cpp
+++ new_passing.cpp
@@ -4,6 +4,9 @@
 
 #include "headers.h"
 
+int pass4(string & winner, string & suitname, player & play, player & nplay);
+
+
 void new_passing(string & winner, string & suitname, player & play, player & wplay, player & nplay, player & eplay)
 {
 	cout << "New Passing ---- " << endl;
@@ -15,7 +18,10 @@ void new_passing(string & winner, string & suitname, player & play, player & wpl
 	
 	if (winner == "Player")
 	{
-		
+		if (!pass4(winner, suitname, play, nplay))
+		{
+			cout << "Error in new_passing, in winner == player loop." << endl;
+		}
 		play.saycards_in_hand();
 		
 		
@@ -63,44 +69,77 @@ int pass4(string & winner, string & suitname, player & play, player & nplay) // 
 	card_array[10] = nplay.card11;
 	card_array[11] = nplay.card12;
 	
+	cout << "This is a check to make sure everything has values: " << endl;
+	
+	int quickcheck1 = 0;
+	cout << "Suitnumber is: " << suittonumber(suitname) << endl;
+	while (quickcheck1 < 12)
+	{
+		
+	cout << card_array[quickcheck1].number << card_array[quickcheck1].name << card_array[quickcheck1].suitnumber << " power: " << card_array[quickcheck1].power << endl;
+	quickcheck1++;
+	}
+	
 	int eligible_cards = 0;
 	
 	card card_array_eligible[12];
 	
-	bool hit_ace_of_trump = false;
+	int hit_array[5];
+	
+	int zero_array_int = 0;
+	while (zero_array_int < 5)
+	{
+		hit_array[zero_array_int] = 0;
+		zero_array_int++;
+	}
+	
+	
 	int hit_ace_of_trump_times = 0;
-	int array_counter = 0;
-	while (array_counter < 12)
-	{
-		if (card_array[array_counter].power == 1 && card_array[array_counter].suitnumber == suittonumber(suitname))
-		{
-			card_array_eligible[eligible_cards + hit_ace_of_trump_times] = card_array[array_counter];
-			hit_ace_of_trump = true;
-			hit_ace_of_trump_times++;
-		}
-	}
-	eligible_cards += hit_ace_of_trump_times;
-	
-	
-	bool hit_ten_of_trump = false;
 	int hit_ten_of_trump_times = 0;
-	array_counter = 0;
-	while (array_counter < 12)
+	int hit_king_of_trump_times = 0;
+	int hit_queen_of_trump_times = 0;
+	int hit_jack_of_trump_times = 0;
+	
+	
+	int array_counter = 0;
+	int card_checking = 0;
+	
+	while (card_checking < 5)
 	{
-		if (card_array[array_counter].power == 2 && card_array[array_counter].suitnumber == suittonumber(suitname))
+		array_counter = 0;
+		
+		while (array_counter < 12)
 		{
-			card_array_eligible[eligible_cards + hit_ten_of_trump_times] = card_array[array_counter];
-			hit_ten_of_trump = true;
-			hit_ten_of_trump_times++;
+			if (card_array[array_counter].power == card_checking && card_array[array_counter].suitnumber == suittonumber(suitname))
+			{
+				
+				if (eligible_cards + hit_array[card_checking] <4)
+				{
+				
+				card_array_eligible[eligible_cards + hit_ace_of_trump_times] = card_array[array_counter];
+				
+				hit_array[card_checking]++;
+				
+				}
+			}
+			
+			array_counter++;
 		}
+		
+		eligible_cards += hit_array[card_checking];
+		cout << "Eligible cards after card " << card_checking << " is " << eligible_cards << endl;
+		card_checking++;
 	}
-	eligible_cards += hit_ten_of_trump_times;
+	
 	
 	if (eligible_cards > 3)
 	{
-		if (eligible_cards > 4) /// This is technically error checking
+		int quick_check_number = 0;
+		
+		while (quick_check_number < 4)
 		{
-			// Fix it :)
+			cout << card_array_eligible[quick_check_number].name << endl;
+			quick_check_number++;
 		}
 	
 		play.pass_possibility1 = card_array_eligible[0].number;
@@ -117,19 +156,10 @@ int pass4(string & winner, string & suitname, player & play, player & nplay) // 
 		return 1;
 	}
 	
-	
-	bool hit_king_of_trump = false;
-	int hit_king_of_trump_times = 0;
-	array_counter = 0;
-	while (array_counter < 12)
-	{
-		if (card_array[array_counter].power == 3 && card_array[array_counter].suitnumber == suittonumber(suitname))
-		{
-			hit_king_of_trump = true;
-			hit_king_of_trump_times++;
-		}
-	}
-	eligible_cards += hit_king_of_trump_times;
+	cout << "Eligible1 : " << card_array_eligible[0].name << endl;
+	cout << "Eligible2 : " << card_array_eligible[1].name << endl;
+	cout << "Eligible3 : " << card_array_eligible[2].name << endl;
+	cout << "Eligible4 : " << card_array_eligible[3].name << endl;
 	
 	return 0; // It failed!
 }
