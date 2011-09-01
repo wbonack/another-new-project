993642018bd8aa51ad8950ebb04b65ba54c00fdd
Westley Bonack <westley@mint>
2011-09-01 14:33:09 -0500

    Fixed the last commits so it is finally a bugfree build.  Disregard the last few commits and don't go back to them!

--- new_passing.cpp
+++ new_passing.cpp
@@ -46,24 +46,26 @@ void new_passing(string & winner, string & suitname, player & play, player & wpl
 int suittonumber(string suit); // In bid.cpp
 
 
-int pass4(string & winner, string & suitname, player & play, player & nplay)
+int pass4(string & winner, string & suitname, player & play, player & nplay) // This does the work of passing four cards across the table
 {
 	card card_array[12];
 	
-	card_array[0] = play.card1;
-	card_array[1] = play.card2;
-	card_array[2] = play.card3;
-	card_array[3] = play.card4;
-	card_array[4] = play.card5;
-	card_array[5] = play.card6;
-	card_array[6] = play.card7;
-	card_array[7] = play.card8;
-	card_array[8] = play.card9;
-	card_array[9] = play.card10;
-	card_array[10] = play.card11;
-	card_array[11] = play.card12;
+	card_array[0] = nplay.card1;
+	card_array[1] = nplay.card2;
+	card_array[2] = nplay.card3;
+	card_array[3] = nplay.card4;
+	card_array[4] = nplay.card5;
+	card_array[5] = nplay.card6;
+	card_array[6] = nplay.card7;
+	card_array[7] = nplay.card8;
+	card_array[8] = nplay.card9;
+	card_array[9] = nplay.card10;
+	card_array[10] = nplay.card11;
+	card_array[11] = nplay.card12;
 	
 	int eligible_cards = 0;
+	
+	card card_array_eligible[12];
 	
 	bool hit_ace_of_trump = false;
 	int hit_ace_of_trump_times = 0;
@@ -72,6 +74,7 @@ int pass4(string & winner, string & suitname, player & play, player & nplay)
 	{
 		if (card_array[array_counter].power == 1 && card_array[array_counter].suitnumber == suittonumber(suitname))
 		{
+			card_array_eligible[eligible_cards + hit_ace_of_trump_times] = card_array[array_counter];
 			hit_ace_of_trump = true;
 			hit_ace_of_trump_times++;
 		}
@@ -84,20 +87,33 @@ int pass4(string & winner, string & suitname, player & play, player & nplay)
 	array_counter = 0;
 	while (array_counter < 12)
 	{
-		if (card_array[array_counter].power == 2 && card_array[array_counter].suit == suittonumber(suitname))
+		if (card_array[array_counter].power == 2 && card_array[array_counter].suitnumber == suittonumber(suitname))
 		{
+			card_array_eligible[eligible_cards + hit_ten_of_trump_times] = card_array[array_counter];
 			hit_ten_of_trump = true;
 			hit_ten_of_trump_times++;
 		}
 	}
 	eligible_cards += hit_ten_of_trump_times;
 	
-	if (eligible > 3)
+	if (eligible_cards > 3)
 	{
-		if (eligible > 4)
+		if (eligible_cards > 4) /// This is technically error checking
 		{
 			// Fix it :)
 		}
+	
+		play.pass_possibility1 = card_array_eligible[0].number;
+		play.pass_possibility2 = card_array_eligible[1].number;
+		play.pass_possibility3 = card_array_eligible[2].number;
+		play.pass_possibility4 = card_array_eligible[3].number;
+		
+		play.pass_possibility_name1 = card_array_eligible[0].name;
+		play.pass_possibility_name2 = card_array_eligible[1].name;
+		play.pass_possibility_name3 = card_array_eligible[2].name;
+		play.pass_possibility_name4 = card_array_eligible[3].name;
+	
+	
 		return 1;
 	}
 	
@@ -105,9 +121,9 @@ int pass4(string & winner, string & suitname, player & play, player & nplay)
 	bool hit_king_of_trump = false;
 	int hit_king_of_trump_times = 0;
 	array_counter = 0;
-	while array_counter < 12)
+	while (array_counter < 12)
 	{
-		if (card_array[array_counter].power == 3 && card_array[array_counter].suit == suittonumber(suitname))
+		if (card_array[array_counter].power == 3 && card_array[array_counter].suitnumber == suittonumber(suitname))
 		{
 			hit_king_of_trump = true;
 			hit_king_of_trump_times++;
